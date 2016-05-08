84c97faadfbc89857891cd4993cf9b57e19a7a68
Dominick <dominickmalzone@aol.com>
2016-05-08 13:00:43 -0700

    added post and validate func

--- boatup/js/controllers.js
+++ boatup/js/controllers.js
@@ -1,10 +1,33 @@
 app.controller('mainCtrl', function($scope) {  
-    console.log("hello");
-
-
-    var firebaseRef = new Firebase("https://boatup.firebaseio.com/boats");
     
 
+	$scope.rtime = Date().slice(0,-23);
+    console.log($scope.rtime);
+    
+
+    var firebaseRef = new Firebase("https://boatup.firebaseio.com/boats");
+    $scope.boats = firebaseRef
+    var postRef = firebaseRef.push();
 
 
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
+   
+    $scope.submitPost = function(){
+    	if (($scope.title !== "") && ($scope.moreinfo !== "")
+	       && ($scope.phone !== "") && ($scope.city !== "")
+    	   && ($scope.price !== "") && (validateEmail($scope.email))){
+	    		postRef.set({Title: $scope.title, Description: $scope.moreinfo, Email: $scope.email, PNumber: $scope.phone, City: $scope.city, Price: $scope.price, Date: $scope.rtime});
+	    		//add load and redirect
+	    		console.log('Sent');
+
+    	} else {
+            $("#allfields").css('display','none');
+            $("#error").css("display", "block");
+        }   
+    };
+    
 });
