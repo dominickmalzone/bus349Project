603f907304f5aaa871daccbabb9b95ed8c48831c
Dominick <dominickmalzone@aol.com>
2016-05-08 21:15:17 -0700

    cleaned up post func

--- boatup/js/controllers.js
+++ boatup/js/controllers.js
@@ -1,27 +1,31 @@
-app.controller('mainCtrl', function($scope) {  
+app.controller('mainCtrl', function($scope, $timeout, $firebaseArray) {  
     
-
 	$scope.rtime = Date().slice(0,-23);
-    console.log($scope.rtime);
     
+    var firebaseRef = new Firebase("https://boatup.firebaseio.com/boat");
 
-    var firebaseRef = new Firebase("https://boatup.firebaseio.com/boats");
-    $scope.boats = firebaseRef
-    var postRef = firebaseRef.push();
-
+    //var postRef = firebaseRef.push();
+    $scope.boats = $firebaseArray(firebaseRef);
 
     function validateEmail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     };
-
    
     $scope.submitPost = function(){
     	if (($scope.title !== "") && ($scope.moreinfo !== "")
 	       && ($scope.phone !== "") && ($scope.city !== "")
     	   && ($scope.price !== "") && (validateEmail($scope.email))){
-	    		postRef.set({Title: $scope.title, Description: $scope.moreinfo, Email: $scope.email, PNumber: $scope.phone, City: $scope.city, Price: $scope.price, Date: $scope.rtime});
-	    		//add load and redirect
+	    	 $scope.boats.$add({
+   				title: $scope.title,
+   				description: $scope.moreinfo,
+   				email: $scope.email,
+   				pnumber: $scope.phone,
+   				city: $scope.city,
+   				price: $scope.price,
+   				date: $scope.rtime
+   			});
+	    		//add loader and redirect
 	    		console.log('Sent');
 
     	} else {
